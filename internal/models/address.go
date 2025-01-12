3733baf0db69e07bc642d2c2339fc1117d184da9
ryan pujo <pujotriantoro@gmail.com>
2025-01-13 01:19:30 +0700

    add validation to address models

--- internal/models/address.go
+++ internal/models/address.go
@@ -2,25 +2,25 @@ package models
 
 type Country struct {
 	ID   uint   `json:"id"`
-	Name string `json:"name"`
+	Name string `json:"name" binding:"required"`
 }
 
 type State struct {
 	ID      uint    `json:"id"`
-	Name    string  `json:"name"`
-	Country Country `json:"country"`
+	Name    string  `json:"name" binding:"required"`
+	Country Country `json:"country" binding:"required"`
 }
 
 type City struct {
 	ID    uint   `json:"id"`
-	Name  string `json:"name"`
-	State State  `json:"state"`
+	Name  string `json:"name" binding:"required"`
+	State State  `json:"state" binding:"required"`
 }
 
 type Address struct {
 	ID          uint   `json:"id"`
-	AddressLine string `json:"address_line"`
-	PostalCode  string `json:"postal_code"`
+	AddressLine string `json:"address_line" binding:"required"`
+	PostalCode  string `json:"postal_code" binding:"required"`
 	IsMain      bool   `json:"is_main"`
-	City        City   `json:"city"`
+	City        City   `json:"city" binding:"required"`
 }
