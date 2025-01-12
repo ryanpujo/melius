5fc0911a42cfc5ebd5941c8c078e2f053320a81b
ryan pujo <pujotriantoro@gmail.com>
2025-01-12 23:23:57 +0700

    refactor according to jwt token changes

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/ryanpujo/melius/application"
 	"github.com/ryanpujo/melius/database"
+	"github.com/ryanpujo/melius/internal/jwttoken"
 	"github.com/ryanpujo/melius/internal/route"
 	"github.com/ryanpujo/melius/registry"
 )
@@ -11,7 +12,7 @@ func main() {
 	db := database.GetDBConnection()
 	defer db.Close()
 	registry := registry.NewRegistry(db)
-	app := application.NewApp(route.SetupRoutes(registry.NewAppControllers()))
+	app := application.NewApp(route.SetupRoutes(registry.NewAppControllers(), jwttoken.GetJWTAuth()))
 
 	if err := app.Serve(); err != nil {
 		panic(err)
