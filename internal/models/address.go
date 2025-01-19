beea003d24fa50339d3e1a2f994f7db1bf877c94
ryan pujo <pujotriantoro@gmail.com>
2025-01-19 21:53:47 +0700

    refactor: add payload structs and enhance model relationships
    - Introduced `CountryPayload`, `StatePayload`, `CityPayload`, and
      `AddressPayload` for API requests.
      - Added nested relationships for `Country`, `State`, `City`, and
        `Address` models.
        - Included lists (`States`, `Cities`, `Addresses`) to represent
          hierarchical relationships.
          - Improved struct organization to separate payloads from main
    	entities.

--- internal/models/address.go
+++ internal/models/address.go
@@ -1,26 +1,54 @@
 package models
 
 type Country struct {
+	ID     uint    `json:"id"`
+	Name   string  `json:"name"`
+	States []State `json:"states"`
+}
+
+type CountryPayload struct {
 	ID   uint   `json:"id"`
 	Name string `json:"name" binding:"required"`
 }
 
 type State struct {
 	ID      uint    `json:"id"`
-	Name    string  `json:"name" binding:"required"`
-	Country Country `json:"country" binding:"required"`
+	Name    string  `json:"name"`
+	Country Country `json:"country"`
+	Cities  []City  `json:"cities"`
+}
+
+type StatePayload struct {
+	ID        uint   `json:"id"`
+	Name      string `json:"name" binding:"required"`
+	CountryID uint   `json:"country_id" binding:"required"`
 }
 
 type City struct {
-	ID    uint   `json:"id"`
-	Name  string `json:"name" binding:"required"`
-	State State  `json:"state" binding:"required"`
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	State     State     `json:"state"`
+	Addresses []Address `json:"addresses"`
+}
+
+type CityPayload struct {
+	ID      uint   `json:"id"`
+	Name    string `json:"name" binding:"required"`
+	StateID uint   `json:"state_id" binding:"required"`
 }
 
 type Address struct {
 	ID          uint   `json:"id"`
+	AddressLine string `json:"address_line"`
+	PostalCode  string `json:"postal_code"`
+	IsMain      bool   `json:"is_main"`
+	City        City   `json:"city"`
+}
+
+type AddressPayload struct {
+	ID          uint   `json:"id"`
 	AddressLine string `json:"address_line" binding:"required"`
 	PostalCode  string `json:"postal_code" binding:"required"`
 	IsMain      bool   `json:"is_main"`
-	City        City   `json:"city" binding:"required"`
+	CityID      uint   `json:"city_id" binding:"required"`
 }
