42634a663c4caa7f098ae2aca12fbba3b269cf1f
ryan pujo <pujotriantoro@gmail.com>
2025-02-03 20:57:43 +0700

    feat(utilities): enhance Response struct with location-based models and optional fields

--- internal/utilities/response.go
+++ internal/utilities/response.go
@@ -1,13 +1,50 @@
 package utilities
 
+import "github.com/ryanpujo/melius/internal/models"
+
 type Response struct {
-	ID      uint   `json:"id,omitempty"`
-	Token   string `json:"token,omitempty"`
-	Err     string `json:"err,omitempty"`
-	Message string `json:"message,omitempty"`
+	ID        uint              `json:"id,omitempty"`
+	Token     string            `json:"token,omitempty"`
+	Country   *models.Country   `json:"country,omitempty"`
+	Countries []*models.Country `json:"countries,omitempty"`
+	State     *models.State     `json:"state,omitempty"`
+	City      *models.City      `json:"city,omitempty"`
+	Address   *models.Address   `json:"address,omitempty"`
+	Err       string            `json:"err,omitempty"`
+	Message   string            `json:"message,omitempty"`
 }
 
 type OptFunc func(*Response)
+
+func WithCountry(country *models.Country) OptFunc {
+	return func(r *Response) {
+		r.Country = country
+	}
+}
+
+func WithCountries(countries []*models.Country) OptFunc {
+	return func(r *Response) {
+		r.Countries = countries
+	}
+}
+
+func WithState(state *models.State) OptFunc {
+	return func(r *Response) {
+		r.State = state
+	}
+}
+
+func WithCity(city *models.City) OptFunc {
+	return func(r *Response) {
+		r.City = city
+	}
+}
+
+func WithAddress(address *models.Address) OptFunc {
+	return func(r *Response) {
+		r.Address = address
+	}
+}
 
 func WithID(id uint) OptFunc {
 	return func(r *Response) {
