56b51e6682dcb35bc5ef61b93dc24dac1806de0c
ryan pujo <pujotriantoro@gmail.com>
2025-02-03 20:55:43 +0700

    feat(config): add support for secondary config file and introduce BaseRoute configuration

--- config/config.go
+++ config/config.go
@@ -1,8 +1,13 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
 
 type Configuration struct {
+	BaseRoute string `mapstructure:"BASE_ROUTE"`
 	DSN       string `mapstructure:"DSN"`
 	Port      int    `mapstructure:"PORT"`
 	JWTKey    string `mapstructure:"JWT_KEY"`
@@ -25,6 +30,11 @@ func readInConfig() {
 
 	if err := viper.ReadInConfig(); err != nil {
 		panic(err)
+	}
+
+	viper.SetConfigName("proof") // secondary file name without extension
+	if err := viper.MergeInConfig(); err != nil {
+		log.Panic("Error merging secondary config file:", err)
 	}
 
 	if err := viper.Unmarshal(&config); err != nil {
